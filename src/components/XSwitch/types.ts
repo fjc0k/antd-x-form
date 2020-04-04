f19e3fe10b9407c031f85f00ffcb83cbe7a1b555
Jay Fong <fjc0kb@gmail.com>
2020-04-05 01:17:05 +0800

    chore: tweaks

--- src/components/XSwitch/types.ts
+++ src/components/XSwitch/types.ts
@@ -2,7 +2,18 @@ import { StrictOmit } from '../../types'
 import { SwitchProps } from 'antd/lib/switch'
 
 export interface XSwitchProps extends SwitchProps {
+  /**
+   * 当前是否选中。
+   *
+   * @default false
+   */
   value?: boolean,
+
+  /**
+   * 初始是否选中。
+   *
+   * @default false
+   */
   defaultValue?: boolean,
 }
 
